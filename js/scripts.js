6626f00e4ba2567e22bd20aa15baab4ac1123af6
willyrabbits <45044417+willyrabbits@users.noreply.github.com>
2021-02-11 10:13:01 +0100

    some face washing

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,25 @@
 const roles = ['a developer', 'a surfer', 'a traveller', 'a runner']
 
 setInterval(function () { randomRole() }, 2000);
+
 var i = 0;
 function randomRole() {
     var iam = document.getElementById("i-am");
     i = i < roles.length - 1 ? i + 1 : 0;
-    iam.innerHTML = roles[i];
+    iam.innerHTML = ''
+    j = 0
+    typeWriter(iam, roles[i])
+}
+
+var j = 0;
+var speed = 50;
+// tryin to make a typing effect, but using setInervat + setTimeout is not a good idea
+function typeWriter(elem, txt) {
+    console.log(txt)
+    if (j < txt.length) {
+        console.log(txt.charAt(j))
+        elem.innerHTML += txt.charAt(j)
+        j++;
+        setTimeout(typeWriter(elem, txt), speed);
+    }
 }
